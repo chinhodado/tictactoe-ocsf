dc2902a653ccefb7590c0e92f096d5518b0d53e6
Chin <chin.bimbo@gmail.com>
2012-11-08 18:52:43 -0500

    initial

---
+++ simplechat1/EchoServer.java
@@ -0,0 +1,271 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+import java.io.*;
+import java.util.Hashtable;
+
+import common.ChatIF;
+
+import ocsf.server.*;
+
+/**
+ * This class overrides some of the methods in the abstract superclass in order to give more functionality to the server.
+ *
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;re
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Paul Holden
+ * @version July 2000
+ */
+public class EchoServer extends AbstractServer {
+    //Class variables *************************************************
+
+    /**
+     * The default port to listen on.
+     */
+    final public static int DEFAULT_PORT = 5555;
+    ChatIF serverUI;//the server console
+    boolean closed = false;//sever closed?
+    boolean stopped = false;//server stopped?
+
+    //Constructors ****************************************************
+    /**
+     * Constructs an instance of the echo server.
+     *
+     * @param port The port number to connect on.
+     */
+    public EchoServer(int port, ChatIF serverUI) //changed for E50b
+    {
+        super(port);
+
+        this.serverUI = serverUI;
+        try {
+            //serverUI.receiveCommand("connected");
+            listen(); //Start listening for connections
+        } catch (Exception ex) {
+            System.out.println("ERROR - Could not listen for clients!");
+        }
+    }
+
+    //Instance methods ************************************************
+    /**
+     * This method handles any messages received from the client.
+     *
+     * @param msg The message received from the client.
+     * @param client The connection from which the message originated.
+     */
+    public void handleMessageFromClient(Object msg, ConnectionToClient client)//changed for E51
+    {
+        if (client.getInfo("numMess").equals(0)) {
+            if (!((String) msg).contains("#login")) {
+                try {
+                    client.sendToClient("SERVER MSG> Error: No login ID provided!");
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                String id = ((String) msg).substring(7);
+                client.setInfo("ID", id);
+                client.setInfo("numMess", 1);
+                System.out.println("Message received " + msg + " from " + id);
+                System.out.println(id + " has logged on");
+                try {
+                    client.sendToClient(id + " has logged on");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+            //Note: E51c/iv is not necessary, since the server cannot receive the #login command at any other time
+            //than when a client connect, since this is already dealt with in ChatClient. 
+        } else {
+            System.out.println("Message received: " + msg + " from " + client + " (" + client.getInfo("ID") + ")");
+            this.sendToAllClients(client.getInfo("ID") + ": " + msg);
+            client.setInfo("numMess", 1);
+            switch (msg.toString()) {
+            case "#1":
+                serverUI.receiveCommand("#1");
+                break;
+            case "#2":
+                serverUI.receiveCommand("#2");
+                break;
+            case "#3":
+                serverUI.receiveCommand("#3");
+                break;
+            case "#4":
+                serverUI.receiveCommand("#4");
+                break;
+            case "#5":
+               serverUI.receiveCommand("#5");
+                break;
+            case "#6":
+                serverUI.receiveCommand("#6");
+                break;
+            case "#7":
+                serverUI.receiveCommand("#7");
+                break;
+            case "#8":
+                serverUI.receiveCommand("#8");
+                break;
+            case "#9":
+                serverUI.receiveCommand("#9");
+                break;
+            default:
+                break;
+        }
+        }
+    }
+
+    /**
+     * This method overrides the one in the superclass. Called when the server starts listening for connections.
+     */
+    protected void serverStarted()//changed for E50c
+    {
+        System.out.println("Server listening for connections on port " + getPort());
+        stopped = false;
+        closed = false;
+    }
+
+    /**
+     * This method overrides the one in the superclass. Called when the server stops listening for connections.
+     */
+    protected void serverStopped()//changed for E50c
+    {
+        System.out.println("Server has stopped listening for connections.");
+        stopped = true;
+    }
+
+    //Class methods ***************************************************
+    /**
+     * This method is responsible for the creation of the server instance (there is no UI in this phase).
+     *
+     * @param args[0] The port number to listen on. Defaults to 5555 if no argument is entered.
+     */
+    //removed for E50b
+    public static void main(String[] args) {
+        int port = 0; //Port to listen on
+
+        try {
+            port = Integer.parseInt(args[0]); //Get port from command line
+        } catch (Throwable t) {
+            port = DEFAULT_PORT; //Set port to 5555
+        }
+
+        EchoServer sv = new EchoServer(port, null);
+
+        try {
+            sv.listen(); //Start listening for connections
+        } catch (Exception ex) {
+            System.out.println("ERROR - Could not listen for clients!");
+        }
+    }
+
+    protected void clientConnected(ConnectionToClient client) {//added for E49c
+        System.out.println("Client " + client + " connected!");
+        client.setInfo("numMess", 0);
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                serverUI.receiveCommand("enableAll");
+            }
+        }, "EnableButton");
+        t.start();
+    }
+
+    synchronized protected void clientDisconnected(ConnectionToClient client) {//added for E49c
+        System.out.println(client.getInfo("ID") + " disconnected!");
+        sendToAllClients(client.getInfo("ID") + " disconnected!");
+    }
+
+    synchronized protected void clientException(ConnectionToClient client, Throwable exception) {//added for E49c
+        System.out.println(client.getInfo("ID") + " disconnected!");
+        sendToAllClients(client.getInfo("ID") + " disconnected!");
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                serverUI.receiveCommand("disableAll");
+            }
+        }, "DisableButton");
+        t.start();
+    }
+
+    void handleMessageFromServerUI(String message) {//added for E50b
+        char temp = message.charAt(0);
+        if (temp == '#') {
+            handleCommand(message);//added for E50
+        } else {
+            sendToAllClients("SERVER MSG> " + message);
+            serverUI.display("SERVER MSG> " + message);
+        }
+    }
+
+    private void handleCommand(String command) {//added for E50c
+        if (command.equals("#quit")) {
+            try {
+                close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.exit(0);
+        } else if (command.equals("#stop")) {
+            stopListening();
+            System.out.println("Listening stopped");
+        } else if (command.equals("#close")) {
+            stopListening();
+            try {
+                close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Server closed");
+        } else if (command.contains("#setport")) {
+            if (closed) {
+                int newPort = Integer.parseInt(command.substring(9));
+                setPort(newPort);
+                System.out.println("Changed port to " + newPort);
+            } else {
+                System.out.println("Server not closed yet!");
+            }
+        } else if (command.equals("#start")) {
+            if (stopped) {
+                try {
+                    listen(); //Start listening for connections
+                } catch (Exception ex) {
+                    System.out.println("ERROR - Could not listen for clients!");
+                }
+            } else {
+                System.out.println("Server not stopped yet!");
+            }
+        } else if (command.equals("#getport")) {
+            System.out.println("Port: " + getPort());
+        } else if (command.equals("#1")) {
+            sendToAllClients("#1");
+        } else if (command.equals("#2")) {
+            sendToAllClients("#2");
+        } else if (command.equals("#3")) {
+            sendToAllClients("#3");
+        } else if (command.equals("#4")) {
+            sendToAllClients("#4");
+        } else if (command.equals("#5")) {
+            sendToAllClients("#5");
+        } else if (command.equals("#6")) {
+            sendToAllClients("#6");
+        } else if (command.equals("#7")) {
+            sendToAllClients("#7");
+        } else if (command.equals("#8")) {
+            sendToAllClients("#8");
+        } else if (command.equals("#9")) {
+            sendToAllClients("#9");
+        } else {
+            System.out.println("Command not recognized!");
+        }
+
+    }
+
+    protected void serverClosed() //added for E50c
+    {
+        closed = true;
+    }
+}
+//End of EchoServer class
---
+++ simplechat1/client/ChatClient.java
@@ -0,0 +1,225 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+package client;
+
+import ocsf.client.*;
+import common.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class overrides some of the methods defined in the abstract superclass in order to give more functionality to the client.
+ *
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @version July 2000
+ */
+public class ChatClient extends AbstractClient {
+    //Instance variables **********************************************
+
+    /**
+     * The interface type variable. It allows the implementation of the display method in the client.
+     */
+    ChatIF clientUI;
+    String id;//login id, added for E51
+
+    //Constructors ****************************************************
+    /**
+     * Constructs an instance of the chat client.
+     *
+     * @param host The server to connect to.
+     * @param port The port number to connect on.
+     * @param clientUI The interface type variable.
+     */
+    public ChatClient(String ID, String host, int port, ChatIF clientUI) //changed for E51
+            throws IOException {
+        super(host, port); //Call the superclass constructor
+        this.id = ID;
+        this.clientUI = clientUI;
+        try {
+            openConnection();
+        } catch (IOException e) {
+            System.out.println("Error: Can't setup connection! Awaiting command");
+            return;
+        }
+        try {
+            sendToServer("#login " + id);
+        } catch (IOException e) {
+            clientUI.display("An error occurred.  Terminating client.");
+            quit();
+        }
+    }
+
+    //Instance methods ************************************************
+    /**
+     * This method handles all data that comes in from the server.
+     *
+     * @param msg The message from the server.
+     */
+    public void handleMessageFromServer(Object msg) {
+        switch (msg.toString()) {
+            case "#1":
+                clientUI.receiveCommand("#1");
+                break;
+            case "#2":
+                clientUI.receiveCommand("#2");
+                break;
+            case "#3":
+                clientUI.receiveCommand("#3");
+                break;
+            case "#4":
+                clientUI.receiveCommand("#4");
+                break;
+            case "#5":
+                clientUI.receiveCommand("#5");
+                break;
+            case "#6":
+                clientUI.receiveCommand("#6");
+                break;
+            case "#7":
+                clientUI.receiveCommand("#7");
+                break;
+            case "#8":
+                clientUI.receiveCommand("#8");
+                break;
+            case "#9":
+                clientUI.receiveCommand("#9");
+                break;
+            case "serverWon":
+                clientUI.receiveCommand("disableAll");    
+                clientUI.receiveCommand("#serverWon");
+                break;
+            case "clientWon":
+                clientUI.receiveCommand("disableAll");
+                clientUI.receiveCommand("#clientWon");
+                break;
+            default:
+                break;
+        }
+        clientUI.display(msg.toString());
+    }
+
+    /**
+     * This method handles all data coming from the UI
+     *
+     * @param message The message from the UI.
+     */
+    public void handleMessageFromClientUI(String message) {
+        char temp = message.charAt(0);
+        if (temp == '#') {
+            try {
+                handleCommand(message);//added for E50
+            } catch (IOException ex) {
+                Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            try {
+                sendToServer(message);
+            } catch (IOException e) {
+                clientUI.display("Could not send message to server.  Terminating client.");
+                quit();
+            }
+        }
+    }
+
+    public void handleCommand(String command) throws IOException {//added for E50
+        if (command.equals("#quit")) {
+            quit();
+        } else if (command.equals("#logoff")) {
+            try {
+                closeConnection();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else if (command.contains("#sethost")) {
+            if (!isConnected()) {
+                String newHost = command.substring(9);
+                setHost(newHost);
+                System.out.println("Changed host to " + newHost);
+            } else {
+                System.out.println("Already connected!");
+            }
+        } else if (command.contains("#setport")) {
+            if (!isConnected()) {
+                int newPort = Integer.parseInt(command.substring(9));
+                setPort(newPort);
+                System.out.println("Changed port to " + newPort);
+            } else {
+                System.out.println("Already connected!");
+            }
+        } else if (command.contains("#login")) {
+            if (!isConnected()) {
+                try {
+                    openConnection();
+                    sendToServer(command);
+                    System.out.println("Connected to server!");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Already connected!");
+            }
+        } else if (command.equals("#gethost")) {
+            System.out.println("Host: " + getHost());
+        } else if (command.equals("#getport")) {
+            System.out.println("Port: " + getPort());
+        } else if (command.equals("#1")) {
+            sendToServer("#1");
+        } else if (command.equals("#2")) {
+            sendToServer("#2");
+        } else if (command.equals("#3")) {
+            sendToServer("#3");
+        } else if (command.equals("#4")) {
+            sendToServer("#4");
+        } else if (command.equals("#5")) {
+            sendToServer("#5");
+        } else if (command.equals("#6")) {
+            sendToServer("#6");
+        } else if (command.equals("#7")) {
+            sendToServer("#7");
+        } else if (command.equals("#8")) {
+            sendToServer("#8");
+        } else if (command.equals("#9")) {
+            sendToServer("#9");
+        } else {
+            System.out.println("Command not recognized!");
+        }
+    }
+
+    /**
+     * This method terminates the client.
+     */
+    public void quit() {
+        try {
+            closeConnection();
+        } catch (IOException e) {
+        }
+        System.exit(0);
+    }
+
+    protected void connectionClosed() { //added for E49a
+        System.out.println("Connection closed");
+    }
+
+    protected void connectionException(Exception exception) { //added for E49a
+        connectionClosed();
+        System.out.println("Server has shutted down.");
+        System.out.println("Terminating client...");
+        System.exit(1);
+    }
+
+    @Override
+    protected void connectionEstablished() {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                clientUI.receiveCommand("enableAll");
+            }
+        }, "EnableButton");
+        t.start();
+    }
+}
